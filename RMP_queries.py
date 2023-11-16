01241c701f611c47ca71ac502bd7fa80f976b65c
Arya Mhaiskar <mhaiskaa@uci.edu>
2023-11-16 12:45:29 -0800

    Finished scraping a professor's rating frequency on RMP

--- RMP_queries.py
+++ RMP_queries.py
@@ -1,3 +1,5 @@
+import requests
+from bs4 import BeautifulSoup
 import ratemyprofessor
 
 UCI_RMP_OBJ = ratemyprofessor.get_school_by_name("UC Irvine")
@@ -7,14 +9,48 @@ class UCI_Prof:
         self._professor = ratemyprofessor.get_professor_by_school_and_name(UCI_RMP_OBJ, name)
     
     def get_avg_difficulty(self):
-        return self._professor.difficulty
+        if self._professor:
+            return self._professor.difficulty
+        else:
+            return "PROFESSOR DOES NOT EXIST"
     
     def get_retake_percentage(self):
-        return self._professor.would_take_again
+        if self._professor:
+            return self._professor.would_take_again
+        else:
+            return "PROFESSOR DOES NOT EXIST"
     
+    def get_tag_freq(self):
+        # Available vars: 'id' (num), 'courses' (course obj), 'name' (full name), 'department', 'difficulty'
+        # 'rating', 'would_take_again', 'num_ratings', 'school' (school obj)
+        if not self._professor:
+            return "PROFESSOR DOES NOT EXIST"
+        url = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid={}'.format(self._professor.id)
+        page = requests.get(url)
+        # Check the HTTP status code
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.text, "html.parser")
+            proftags = soup.findAll("span", {"class": "Tag-bs9vf4-0 hHOVKF" })
+            tag_freq = {}
+
+            numTags = 0
+            for mytag in proftags:
+                if (numTags >= 5):
+                    if mytag.getText() in tag_freq:
+                        tag_freq[mytag.getText()] += 1
+                    else:
+                        tag_freq[mytag.getText()] = 1
+                numTags += 1
+            
+            return tag_freq
+        else:
+            return "ERROR SCRAPING TAGS"
+
+
 
 # Basic testing
 if __name__ == '__main__':
-    shindler = UCI_Prof("Michael Shindler")
-    print("Avg difficulty: ", shindler.get_avg_difficulty())
-    print("Retake rate: ", shindler.get_retake_percentage(), "%")
+    gopi = UCI_Prof("Gopi Meenakshisundaram")
+    print("Avg difficulty: ", gopi.get_avg_difficulty())
+    print("Retake rate: ", gopi.get_retake_percentage(), "%")
+    print(gopi.get_tag_freq())
